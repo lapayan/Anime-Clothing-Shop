fcff488eb1c989c2e0ded23dd44f2ee3f86167a2
bigsexy <luispayan2001@gmail.com>
2023-07-20 07:56:12 -0600

    Fixed appearances

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,34 +5,34 @@ const categories = [
   {
     id:1,
     title: 'Hats',
-    imgUrl:'https://bit.ly/3NGnxCU',
+    imgUrl:'https://shutupandtakemyyen.com/wp-content/uploads/2018/12/One-Piece-Luffy-Straw-Hat.jpeg',
     route: 'shop/hats'
   },
   {
     id:2,
     title: 'Jackets',
-    imgUrl: 'https://bit.ly/3QjmXga',
+    imgUrl: 'https://i.ebayimg.com/images/g/pE8AAOSwSF9gWax1/s-l1200.jpg',
     route: 'shop/jackets'
   },
 
   {
     id:3,
-    title: 'Sneakers',
-    imgUrl: 'https://bit.ly/3xDW5Qn',
+    title: 'Bags',
+    imgUrl: 'https://www.pennycraftsstudio.com/cdn/shop/products/O1CN01nY4yHe1HWPi6S4LC5__2209103660765_1200x1200.jpg?v=1657677345',
     route: 'shop/sneakers'
   },
 
   {
     id:4,
     title: 'Womens',
-    imgUrl: 'https://bit.ly/3QbzWQO',
+    imgUrl: 'https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/07/Anime-Cosplay.jpg',
     route: 'shop/womens'
   },
 
   {
     id:5,
     title: 'Mens',
-    imgUrl: 'https://bit.ly/3NFZPGX',
+    imgUrl: 'https://pbs.twimg.com/media/ESCQQftWsAYv3ym.jpg:large',
     route: 'shop/mens'
 
   },
